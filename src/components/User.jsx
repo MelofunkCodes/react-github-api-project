40f7f6ace7f98ab85a7a21fcd5f6bd3eaf87d341
MelofunkCodes <myeoh27@gmail.com>
2017-03-01 13:34:25 -0500

    updated from test

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -24,7 +24,6 @@ class User extends React.Component {
     }
 
     fetchData(){
-        //yo
 
         fetch(`https://api.github.com/users/${this.props.params.username}`)
         .then(response => response.json())
